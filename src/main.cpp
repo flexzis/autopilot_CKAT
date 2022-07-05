b748f543763d3fdc5f7f8f3546a8314981144947
target30 <korsunvladimir22@mail.ru>
2022-07-05 22:53:37 +0300

    работает!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,9 @@
-
-#include "../lib/sensors/MPU9250-master/MPU9250.h"
 #include <Arduino.h>
+#include "MPU9250.h"
+
 
 
 MPU9250 mpu;
-
-void setup() {
-    Serial.begin(115200);
-    Wire.begin();
-    delay(2000);
-
-    if (!mpu.setup(0x68)) {  // change to your own address
-        while (1) {
-            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
-            delay(5000);
-        }
-    }
-
-    // calibrate anytime you want to
-    Serial.println("Accel Gyro calibration will start in 5sec.");
-    Serial.println("Please leave the device still on the flat plane.");
-    mpu.verbose(true);
-    delay(5000);
-    mpu.calibrateAccelGyro();
-
-    Serial.println("Mag calibration will start in 5sec.");
-    Serial.println("Please Wave device in a figure eight until done.");
-    delay(5000);
-    mpu.calibrateMag();
-
-    print_calibration();
-    mpu.verbose(false);
-}
-
-void loop() {
-    if (mpu.update()) {
-        static uint32_t prev_ms = millis();
-        if (millis() > prev_ms + 25) {
-            print_roll_pitch_yaw();
-            prev_ms = millis();
-        }
-    }
-}
 
 void print_roll_pitch_yaw() {
     Serial.print("Yaw, Pitch, Roll: ");
@@ -82,4 +44,43 @@ void print_calibration() {
     Serial.print(", ");
     Serial.print(mpu.getMagScaleZ());
     Serial.println();
+}
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    delay(2000);
+
+    if (!mpu.setup(0x68)) {  // cohange to your own address
+        while (1) {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+    }
+
+    // calibrate anytime you want to
+    Serial.println("Accel Gyro calibration will start in 5sec.");
+    Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    delay(5000);
+    mpu.calibrateAccelGyro();
+
+    Serial.println("Mag calibration will start in 5sec.");
+    Serial.println("Please Wave device in a figure eight until done.");
+    delay(5000);
+    mpu.calibrateMag();
+
+    print_calibration();
+    mpu.verbose(false);
+}
+
+void loop() {
+    if (mpu.update()) {
+        static uint32_t prev_ms = millis();
+        if (millis() > prev_ms + 25) {
+            print_roll_pitch_yaw();
+            prev_ms = millis();
+            Serial.flush();
+        }
+    }
 }
