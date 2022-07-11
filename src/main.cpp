516741103fe0d36297bf501e6f19b8d75f0796a8
Plotnikov Grigory <grobotzuk@gmail.com>
2022-07-11 18:33:52 +0300

    minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,6 @@
 //#include "PID.h"
 #include "Accelerometer.h"
 
-class PID
-{
-    double kp, kd, ki;
-    double min_err, max_err;
-    double dt;
-    int N;
-    
-    public:
-    PID(double kp, double kd, double ki,
-        double min_err=0., double max_err=100.,
-        double dt = 0.001, int N = 10)
-    : kp {kp}, kd {kd}, ki {ki}
-    , min_err {min_err}, max_err {max_err}
-    , dt {dt}, N{N}
-    {}
-
-    double calculate(double val, double des_val)
-    {
-        double err = val - des_val;
-        return kp * err;
-    }
-};
 MPU mpu;
 Servo aileron_l;
 Servo aileron_r;
